56b12c4f2d87e1dff295aa0369fce3c448678edc
Nick Ball <nick@wolfninja.com>
2014-12-20 18:23:37 -0800

    Fix integration test reporting, add simple unit test

---
+++ src/test/java/com/wolfninja/keystore/redis/RedisAdapterTest.java
@@ -0,0 +1,32 @@
+package com.wolfninja.keystore.redis;
+
+import org.easymock.EasyMock;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import redis.clients.jedis.JedisPool;
+
+import com.wolfninja.keystore.api.Keyspace;
+
+@Test
+public class RedisAdapterTest {
+
+	@Test
+	public void create() {
+		final JedisPool mockPool = EasyMock.createMock(JedisPool.class);
+
+		final RedisAdapter actual = RedisAdapter.create(mockPool);
+
+		Assert.assertNotNull(actual);
+	}
+
+	@Test
+	public void getKeyspace() {
+		final JedisPool mockPool = EasyMock.createMock(JedisPool.class);
+
+		final RedisAdapter adapter = RedisAdapter.create(mockPool);
+		final Keyspace actual = adapter.getKeyspace("my.keyspace");
+
+		Assert.assertEquals(actual.getClass(), RedisKeyspace.class);
+	}
+}
