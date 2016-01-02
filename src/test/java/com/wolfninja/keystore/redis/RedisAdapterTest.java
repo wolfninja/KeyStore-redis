d24a4d093472d03ad9e95cc6cff235a0f2f44c92
Nick Ball <nick@nickball.net>
2016-01-01 20:42:17 -0800

    Add test coverage for prefix

--- src/test/java/com/wolfninja/keystore/redis/RedisAdapterTest.java
+++ src/test/java/com/wolfninja/keystore/redis/RedisAdapterTest.java
@@ -1,16 +1,36 @@
 package com.wolfninja.keystore.redis;
 
+import java.util.Optional;
+
 import org.easymock.EasyMock;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import redis.clients.jedis.JedisPool;
-
 import com.wolfninja.keystore.api.Keyspace;
+
+import redis.clients.jedis.JedisPool;
 
 @SuppressWarnings("javadoc")
 @Test
 public class RedisAdapterTest {
+
+	@Test(dataProvider = "buildKeyspaceNameData")
+	public void buildKeyspaceName(final String inputPrefix, final String expected) {
+		final JedisPool mockPool = EasyMock.createMock(JedisPool.class);
+
+		final RedisAdapter adapter = RedisAdapter.create(mockPool, inputPrefix);
+		final String actual = adapter.buildKeyspaceName("myKeyspace");
+		Assert.assertEquals(actual, expected);
+	}
+
+	@DataProvider
+	Object[][] buildKeyspaceNameData() {
+		return new Object[][] { //
+				{ null, "myKeyspace" }, //
+				{ "nice_", "nice_myKeyspace" }, //
+		};
+	}
 
 	@Test
 	public void create() {
@@ -19,6 +39,16 @@ public class RedisAdapterTest {
 		final RedisAdapter actual = RedisAdapter.create(mockPool);
 
 		Assert.assertNotNull(actual);
+	}
+
+	@Test
+	public void createWithPrefix() {
+		final JedisPool mockPool = EasyMock.createMock(JedisPool.class);
+
+		final RedisAdapter actual = RedisAdapter.create(mockPool, "asdf_");
+
+		Assert.assertNotNull(actual);
+		Assert.assertEquals(actual.getKeyspacePrefix(), Optional.of("asdf_"));
 	}
 
 	@Test
