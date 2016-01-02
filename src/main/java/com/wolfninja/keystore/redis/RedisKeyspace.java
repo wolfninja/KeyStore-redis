5eac607bc611115d17bad694e9f13647035124b5
Nick Ball <nick@nickball.net>
2016-01-01 22:57:33 -0800

    Fix jedis usage, transactions for non-atomic operations

--- src/main/java/com/wolfninja/keystore/redis/RedisKeyspace.java
+++ src/main/java/com/wolfninja/keystore/redis/RedisKeyspace.java
@@ -1,5 +1,6 @@
 package com.wolfninja.keystore.redis;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import com.wolfninja.keystore.api.Keyspace;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 /**
  * Keyspace implementation for Redis
@@ -43,23 +45,30 @@ public class RedisKeyspace implements Keyspace {
 		Objects.requireNonNull(key, "Key should be provided");
 		Objects.requireNonNull(value, "Value should be provided");
 
-		return jedisInstance().hsetnx(keyspaceName, key, value) == 1;
+		try (final Jedis jedis = jedisInstance()) {
+			return jedis.hsetnx(keyspaceName, key, value) == 1;
+		}
 	}
 
 	@Override
 	public boolean checkAndSet(final String key, final String value, long version) {
-		// FIXME needs transaction support
 		Objects.requireNonNull(key, "Key should be provided");
 		Objects.requireNonNull(value, "Value should be provided");
 
-		final String existingValue = jedisInstance().hget(keyspaceName, key);
-
-		if (existingValue != null) {
-			final int existingVersion = existingValue.hashCode();
-			if (existingVersion == version) {
-				jedisInstance().hset(keyspaceName, key, value);
-				return true;
+		try (final Jedis jedis = jedisInstance()) {
+			// FIXME right now we are locking on the entire keyspace/hash (issue #1)
+			jedis.watch(keyspaceName);
+			final String existingValue = jedis.hget(keyspaceName, key);
+			final Transaction trans = jedis.multi();
+			if (existingValue != null) {
+				final int existingVersion = existingValue.hashCode();
+				if (existingVersion == version) {
+					trans.hset(keyspaceName, key, value);
+					final List<Object> response = trans.exec();
+					return response != null && response.size() == 1 && response.get(0).equals(0L);
+				}
 			}
+			trans.discard();
 		}
 		return false;
 	}
@@ -67,22 +76,30 @@ public class RedisKeyspace implements Keyspace {
 	@Override
 	public boolean delete(final String key) {
 		Objects.requireNonNull(key, "Key should be provided");
-		return jedisInstance().hdel(keyspaceName, key) == 1;
+
+		try (final Jedis jedis = jedisInstance()) {
+			return jedis.hdel(keyspaceName, key) == 1;
+		}
 	}
 
 	@Override
 	public boolean deletes(final String key, final long version) {
-		// FIXME needs transaction support
 		Objects.requireNonNull(key, "Key should be provided");
 
-		final String existingValue = jedisInstance().hget(keyspaceName, key);
-
-		if (existingValue != null) {
-			final int existingVersion = existingValue.hashCode();
-			if (existingVersion == version) {
-				jedisInstance().hdel(keyspaceName, key);
-				return true;
+		try (final Jedis jedis = jedisInstance()) {
+			// FIXME right now we are locking on the entire keyspace/hash (issue #1)
+			jedis.watch(keyspaceName);
+			final String existingValue = jedis.hget(keyspaceName, key);
+			final Transaction trans = jedis.multi();
+			if (existingValue != null) {
+				final int existingVersion = existingValue.hashCode();
+				if (existingVersion == version) {
+					trans.hdel(keyspaceName, key);
+					final List<Object> result = trans.exec();
+					return result != null && result.size() == 1 && result.get(0).equals(1L);
+				}
 			}
+			trans.discard();
 		}
 		return false;
 
@@ -92,23 +109,29 @@ public class RedisKeyspace implements Keyspace {
 	public boolean exists(final String key) {
 		Objects.requireNonNull(key, "Key should be provided");
 
-		return jedisInstance().hexists(keyspaceName, key);
+		try (final Jedis jedis = jedisInstance()) {
+			return jedis.hexists(keyspaceName, key);
+		}
 	}
 
 	@Override
 	public Optional<String> get(final String key) {
 		Objects.requireNonNull(key, "Key should be provided");
 
-		return Optional.ofNullable(jedisInstance().hget(keyspaceName, key));
+		try (final Jedis jedis = jedisInstance()) {
+			return Optional.ofNullable(jedis.hget(keyspaceName, key));
+		}
 	}
 
 	@Override
 	public Optional<KeyValue> gets(final String key) {
 		Objects.requireNonNull(key, "Key should be provided");
 
-		final String value = jedisInstance().hget(keyspaceName, key);
-		final KeyValue keyValue = (value == null) ? null : KeyValue.create(key, value, value.hashCode());
-		return Optional.ofNullable(keyValue);
+		try (final Jedis jedis = jedisInstance()) {
+			final String value = jedis.hget(keyspaceName, key);
+			final KeyValue keyValue = (value == null) ? null : KeyValue.create(key, value, value.hashCode());
+			return Optional.ofNullable(keyValue);
+		}
 	}
 
 	private Jedis jedisInstance() {
@@ -117,18 +140,22 @@ public class RedisKeyspace implements Keyspace {
 
 	@Override
 	public boolean replace(final String key, final String value) {
-		// FIXME needs transaction support
 		Objects.requireNonNull(key, "Key should be provided");
 		Objects.requireNonNull(value, "Value should be provided");
 
-		final Jedis resource = jedisInstance();
-
-		final String existing = resource.hget(keyspaceName, key);
-		if (existing != null) {
-			if (value.hashCode() != existing.hashCode()) {
-				resource.hset(keyspaceName, key, value);
-				return true;
+		try (final Jedis jedis = jedisInstance()) {
+			// FIXME right now we are locking on the entire keyspace/hash (issue #1)
+			jedis.watch(keyspaceName);
+			final String existing = jedis.hget(keyspaceName, key);
+			final Transaction trans = jedis.multi();
+			if (existing != null) {
+				if (value.hashCode() != existing.hashCode()) {
+					trans.hset(keyspaceName, key, value);
+					final List<Object> response = trans.exec();
+					return response != null && response.size() == 1 && response.get(0).equals(0L);
+				}
 			}
+			trans.discard();
 		}
 		return false;
 	}
@@ -138,8 +165,10 @@ public class RedisKeyspace implements Keyspace {
 		Objects.requireNonNull(key, "Key should be provided");
 		Objects.requireNonNull(value, "Value should be provided");
 
-		jedisInstance().hset(keyspaceName, key, value);
-		return true;
+		try (final Jedis jedis = jedisInstance()) {
+			jedis.hset(keyspaceName, key, value);
+			return true;
+		}
 	}
 
 }
