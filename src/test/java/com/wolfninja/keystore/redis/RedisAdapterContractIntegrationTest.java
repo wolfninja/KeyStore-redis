71bb68f390ef485c9fcb394bd88ebf2aed99fd57
Nick Ball <nick@nickball.net>
2016-01-01 22:56:35 -0800

    switch to embedded redis for integration tests

--- src/test/java/com/wolfninja/keystore/redis/RedisAdapterContractIntegrationTest.java
+++ src/test/java/com/wolfninja/keystore/redis/RedisAdapterContractIntegrationTest.java
@@ -1,16 +1,43 @@
 package com.wolfninja.keystore.redis;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import redis.clients.jedis.JedisPoolConfig;
-
-import com.fiftyonred.mock_jedis.MockJedisPool;
 import com.wolfninja.keystore.api.BaseKeyValueStoreAdapterTest;
 
+import redis.clients.jedis.JedisPool;
+import redis.embedded.RedisServer;
+import redis.embedded.ports.EphemeralPortProvider;
+
+@SuppressWarnings("javadoc")
 @Test
 public class RedisAdapterContractIntegrationTest extends BaseKeyValueStoreAdapterTest {
 
+	private static RedisServer server;
+	private static int port;
+
+	private static int genPort() {
+		final int port = new EphemeralPortProvider().next();
+		RedisAdapterContractIntegrationTest.port = port;
+		System.out.println("Using port: " + port);
+		return port;
+	}
+
 	public RedisAdapterContractIntegrationTest() {
-		super(new RedisAdapter(new MockJedisPool(new JedisPoolConfig(), "127.0.0.1")));
+		super(new RedisAdapter(new JedisPool("localhost", genPort())));
+	}
+
+	@BeforeClass
+	public void setUp() {
+		server = RedisServer.builder().port(port).build();
+		server.start();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (server != null && server.isActive()) {
+			server.stop();
+		}
 	}
 }
--- src/test/java/com/wolfninja/keystore/redis/RedisKeyspaceContractIntegrationTest.java
+++ src/test/java/com/wolfninja/keystore/redis/RedisKeyspaceContractIntegrationTest.java
@@ -1,16 +1,43 @@
 package com.wolfninja.keystore.redis;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import redis.clients.jedis.JedisPoolConfig;
-
-import com.fiftyonred.mock_jedis.MockJedisPool;
 import com.wolfninja.keystore.api.BaseKeyspaceTest;
 
+import redis.clients.jedis.JedisPool;
+import redis.embedded.RedisServer;
+import redis.embedded.ports.EphemeralPortProvider;
+
+@SuppressWarnings("javadoc")
 @Test
 public class RedisKeyspaceContractIntegrationTest extends BaseKeyspaceTest {
 
+	private static RedisServer server;
+	private static int port;
+
+	private static int genPort() {
+		final int port = new EphemeralPortProvider().next();
+		RedisKeyspaceContractIntegrationTest.port = port;
+		System.out.println("Using port: " + port);
+		return port;
+	}
+
 	public RedisKeyspaceContractIntegrationTest() {
-		super(new RedisAdapter(new MockJedisPool(new JedisPoolConfig(), "127.0.0.1")).getKeyspace("awesome.keyspace"));
+		super(new RedisAdapter(new JedisPool("localhost", genPort())).getKeyspace("awesome.keyspace"));
+	}
+
+	@BeforeClass
+	public void setUp() {
+		server = RedisServer.builder().port(port).build();
+		server.start();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (server != null && server.isActive()) {
+			server.stop();
+		}
 	}
 }
